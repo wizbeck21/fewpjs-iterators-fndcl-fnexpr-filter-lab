8d0501d522e1ac4e7d4df6e1705da2e3c2bd0f1d
Wesley <wes.beck21@outlook.com>
2021-02-27 12:35:27 -0800

    commit tree 3b50fed224e642bc442bca5bdef004333efeee38

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, string) {
+  let matchingDrivers = drivers.filter(d => {
+    return d.toLowerCase() === string.toLowerCase()
+  })
+  return matchingDrivers
+}
+
+function fuzzyMatch(drivers, string) {
+  let fuzzyMatches = drivers.filter(d => {
+    return d.toLowerCase().indexOf(string.toLowerCase()) === 0
+  })
+  return fuzzyMatches
+}
+
+function matchName(drivers, string){
+  return drivers.filter(d => d.name === string)
+}
